4f3bd552e530af09b2fb7f889e0e5d49c4fb54ac
Aniketh Hotagi <aniketh.hotagi@gmail.com>
2020-11-05 21:52:02 +0530

    Added Loading Page and Icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,89 +1,114 @@
-import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
-import { makeStyles } from '@material-ui/core'
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
 
-import EBookListing from './Components/EBookListing'
-import Navcomponent from './Components/Navcomponent'
-import Homepage from './Components/pages/Homepage'
-import { portis } from './drizzleOptions'
-import './App.css'
-import PublishBook from './Components/PublishBook'
+import EBookListing from "./Components/EBookListing";
+import Navcomponent from "./Components/Navcomponent";
+import Homepage from "./Components/pages/Homepage";
+import { portis } from "./drizzleOptions";
 
+import LoadingImg from "./Components/Loading";
 
-const ipfsClient = require('ipfs-http-client')
-const ipfs = ipfsClient({host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+import "./App.css";
+import PublishBook from "./Components/PublishBook";
+
+const ipfsClient = require("ipfs-http-client");
+const ipfs = ipfsClient({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+});
 
 //to add files to ipfs when using it, under event handler.
 /*ipfs.add(this.state.buffer, (error,result) => {
 	//do stuff here...
 })*/
 
-const App = props => {
-	const classes = useStyles()
+const App = (props) => {
+  const classes = useStyles();
 
-	const [walletAddress, setWalletAddress] = useState()
-	const [email, setEmail] = useState()
-	const [reputation, setReputation] = useState()
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		// const web3 = props.drizzle.web3
-		// web3.eth.getAccounts().then(accounts => {
-		// 	this.account = accounts[0]
-		// })
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000);
+  }, []);
 
-		portis.onLogin((walletAddress, email, reputation) => {
-			setWalletAddress(walletAddress)
-			setEmail(email)
-			setReputation(reputation)
+  const [walletAddress, setWalletAddress] = useState();
+  const [email, setEmail] = useState();
+  const [reputation, setReputation] = useState();
 
-			// console.log(walletAddress, email, reputation)
-		})
+  useEffect(() => {
+    // const web3 = props.drizzle.web3
+    // web3.eth.getAccounts().then(accounts => {
+    // 	this.account = accounts[0]
+    // })
 
-		portis.onLogout(() => {
-			setWalletAddress('')
-			setEmail('')
-			setReputation('')
+    portis.onLogin((walletAddress, email, reputation) => {
+      setWalletAddress(walletAddress);
+      setEmail(email);
+      setReputation(reputation);
 
-			console.log('User logged out')
-		})
-	}, [])
+      // console.log(walletAddress, email, reputation)
+    });
 
-	return (
-		<Router>
-			<div className={classes.navigation}>
-				<Navcomponent walletAddress={walletAddress} email={email} reputation={reputation} />
-			</div>
-			<div className='container' style={{ display: 'flex', flexDirection: 'column' }}>
-				<Switch className='route-wrapper'>
-					<Route exact path='/'>
-						<Homepage {...props} />
-					</Route>
-					<Route exact path='/ebooks'>
-						<EBookListing {...props} />
-					</Route>
-					<Route exact path='/publish'>
-						<PublishBook {...props} />
-					</Route>
-					<Route exact path='/purchase'>
-						<EBookListing {...props} />
-					</Route>
-					<Route exact path='/clientLibrary'>
-						<EBookListing {...props} />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
-	)
-}
+    portis.onLogout(() => {
+      setWalletAddress("");
+      setEmail("");
+      setReputation("");
 
-const useStyles = makeStyles(theme => ({
-	navigation: {
-		marginTop: 0,
-		marginBottom: 'auto',
-		position: 'fixed',
-		width: '100%',
-		height: 63.99,
-	},
-}))
+      console.log("User logged out");
+    });
+  }, []);
 
-export default App
+  return (
+    <>
+      {loading === false ? (
+        <Router>
+          <div className={classes.navigation}>
+            <Navcomponent
+              walletAddress={walletAddress}
+              email={email}
+              reputation={reputation}
+            />
+          </div>
+          <div
+            className="container"
+            style={{ display: "flex", flexDirection: "column" }}
+          >
+            <Switch className="route-wrapper">
+              <Route exact path="/">
+                <Homepage {...props} />
+              </Route>
+              <Route exact path="/ebooks">
+                <EBookListing {...props} />
+              </Route>
+              <Route exact path="/publish">
+                <PublishBook {...props} />
+              </Route>
+              <Route exact path="/purchase">
+                <EBookListing {...props} />
+              </Route>
+              <Route exact path="/clientLibrary">
+                <EBookListing {...props} />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      ) : (
+        <LoadingImg />
+      )}
+    </>
+  );
+};
+
+const useStyles = makeStyles((theme) => ({
+  navigation: {
+    marginTop: 0,
+    marginBottom: "auto",
+    position: "fixed",
+    width: "100%",
+    height: 63.99,
+  },
+}));
+
+export default App;
