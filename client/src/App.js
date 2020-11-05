3e684b3f2bfa5e14ae299cd37963dce33caed335
abhishek-99ks <abhishek.99ks@gmail.com>
2020-11-05 09:34:43 +0530

    Added new folder for every page and modified css

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import MyComponent from "./MyComponent";
 import Navcomponent from "./Components/Navcomponent"
 import Leftbar from "./Components/Leftbar"
 import Bookcard from "./Components/Bookcard"
+import Homepage from "./Components/pages/Homepage"
 import "./App.css";
 
 
@@ -14,9 +15,6 @@ const drizzle = new Drizzle(drizzleOptions);
 const App = () => {
   return (
     <>
-      <div className="navigation">
-        <Navcomponent />
-      </div>
       <div className='container' style={{display:"flex", flexDirection: "column"}}>
         <DrizzleContext.Provider drizzle={drizzle}>
           <DrizzleContext.Consumer>
@@ -34,32 +32,9 @@ const App = () => {
           </DrizzleContext.Consumer>
         </DrizzleContext.Provider>
         
-        <div className="content">
-          <div className="leftbar">
-            <Leftbar />
-          </div>
-          <div className="books">
-            <h3>Latest Books</h3>
-            <hr style={{margin: "0 20px 10px 20px"}}/>
-            <div className="bookcard">
-              <Bookcard />
-              <Bookcard />
-              <Bookcard />
-              <Bookcard />
-              <Bookcard />
-            </div>
-            <h3>Explore Books</h3>
-            <hr style={{margin: "0 20px 10px 20px"}}/>
-            <div className="bookcard">
-              <Bookcard />
-              <Bookcard />
-              <Bookcard />
-              <Bookcard />
-              <Bookcard />
-            </div>
-          </div>
-        </div>
+        <Homepage />
       </div>
+      
     </>
     
     
---
+++ client/src/Components/pages/Homepage.js
@@ -0,0 +1,100 @@
+import React from 'react'
+
+import Navcomponent from "../Navcomponent"
+import Leftbar from "../Leftbar"
+import Bookcard from "../Bookcard"
+import { makeStyles } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+    container: {
+  height: '100vh',
+},
+
+  content: {
+  display: 'flex',
+  paddingTop: '63.99px',
+  height: '100vh',
+  overflow: 'hidden',
+},
+
+leftbar: {
+  position: 'fixed',
+  overflowX: 'hidden',
+},
+
+books: {
+  overflow: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+  marginLeft: 300,
+  marginRight: 'auto',
+  padding: "0 auto",
+},
+
+h3: {
+  marginLeft: 20,
+  marginBottom: 0,
+},
+
+bookcard: {
+  display: 'flex',
+  height: 250,
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  paddingBottom: 20,
+},
+
+navigation: {
+  marginTop: 0,
+  marginBottom: 'auto',
+  position: 'fixed',
+  width: '100%',
+  height: 63.99,
+}
+})
+
+
+
+const Homepage = () => {
+
+    const classes = useStyles();
+
+    return (
+        <div>
+            <div className={classes.navigation}>
+                <Navcomponent />
+            </div>
+            <div className={classes.container} style={{display:"flex", flexDirection: "column"}}>
+                <div className={classes.content}>
+                    <div className={classes.leftbar}>
+                        <Leftbar />
+                    </div>
+                    <div className={classes.books}>
+                        <h3 className={classes.h3}>Latest Books</h3>
+                        <hr style={{margin: "0 20px 10px 20px"}}/>
+                        <div className={classes.bookcard}>
+                            <Bookcard />
+                            <Bookcard />
+                            <Bookcard />
+                            <Bookcard />
+                            <Bookcard />
+                        </div>
+                        <h3 className={classes.h3}>Explore Books</h3>
+                        <hr style={{margin: "0 20px 10px 20px"}}/>
+                        <div className={classes.bookcard}>
+                            <Bookcard />
+                            <Bookcard />
+                            <Bookcard />
+                            <Bookcard />
+                            <Bookcard />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
