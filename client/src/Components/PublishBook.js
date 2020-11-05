16b855641d7aa30b59bba5e329fab69d26273584
Abdul Mateen <abdulmateen075@gmail.com>
2020-11-06 02:33:27 +0530

    Update Publish Book UI

--- client/src/Components/PublishBook.js
+++ client/src/Components/PublishBook.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react'
 import { newContextComponents } from '@drizzle/react-components'
 import Button from '@material-ui/core/Button'
 import CloudUploadIcon from '@material-ui/icons/CloudUpload'
+import Snackbar from '@material-ui/core/Snackbar'
+import MuiAlert from '@material-ui/lab/Alert'
 
 import DrizzleForm from './pages/DrizzleForm'
 import ipfs from '../ipfs'
-import { IconButton, makeStyles } from '@material-ui/core'
+import { Container, Divider, IconButton, makeStyles, Paper, Typography } from '@material-ui/core'
+
+function Alert(props) {
+	return <MuiAlert elevation={6} variant='filled' {...props} />
+}
 
 const { AccountData, ContractData, ContractForm } = newContextComponents
 
@@ -16,6 +22,24 @@ export default function PublishBook({ drizzle, drizzleState, initialized }) {
 	const [epubFile, setEpubFile] = useState()
 	const [ipfsHash, setIpfsHash] = useState()
 	const [buffer, setBuffer] = useState('')
+	const [uploadOpen, setUploadOpen] = useState(false)
+	const [publishOpen, setPublishOpen] = useState(false)
+
+	const handleCloseUpload = (event, reason) => {
+		if (reason === 'clickaway') {
+			return
+		}
+
+		setUploadOpen(false)
+	}
+
+	const handleClosePublish = (event, reason) => {
+		if (reason === 'clickaway') {
+			return
+		}
+
+		setPublishOpen(false)
+	}
 
 	const captureFile = event => {
 		event.stopPropagation()
@@ -34,13 +58,14 @@ export default function PublishBook({ drizzle, drizzleState, initialized }) {
 		setBuffer(buffer)
 	}
 
-	const onSubmit = async (event) => {
+	const onSubmit = async event => {
 		event.preventDefault()
 
 		console.log('on submit')
 		await ipfs.add(buffer, (err, ipfsHash) => {
 			console.log(err, ipfsHash)
 			setIpfsHash(ipfsHash[0].hash)
+			setUploadOpen(true)
 		})
 	}
 
@@ -49,74 +74,118 @@ export default function PublishBook({ drizzle, drizzleState, initialized }) {
 	}
 
 	return (
-		<div className={classes.container}>
-			<DrizzleForm
-				ipfsHash={ipfsHash}
-				method='publishBook'
-				drizzle={drizzle}
-				drizzleState={drizzleState}
-				initialized={initialized}>
-				<div>
-					<AccountData
-						drizzle={drizzle}
-						drizzleState={drizzleState}
-						accountIndex={0}
-						units='ether'
-						precision={3}
-						render={({ address, balance, units }) => (
-							<div>
+		<Container maxWidth='lg' className={classes.container}>
+			<div style={{ marginBottom: 20 }}>
+				<AccountData
+					drizzle={drizzle}
+					drizzleState={drizzleState}
+					accountIndex={0}
+					units='ether'
+					precision={3}
+					render={({ address, balance, units }) => (
+						<>
+							<Typography variant='h6'>
 								My Address: <span style={{ color: 'red' }}>{address}</span> <br />
-								MATIC Token: <span style={{ color: 'red' }}>{balance}</span> {units}
-								My DBKT: <span style={{ color: 'red' }}>{dbktBalance}</span> {units}
-							</div>
-						)}
+							</Typography>
+							<Typography variant='h6'>
+								MATIC Token: <span style={{ color: 'red' }}>{balance}</span>
+							</Typography>
+							<Typography variant='h6'>
+								My DBKT: <span style={{ color: 'red' }}>{dbktBalance}</span>
+							</Typography>
+						</>
+					)}
+				/>
+			</div>
+
+			<Divider />
+
+			<div className={classes.formContainer}>
+				<Typography variant='h2' style={{ fontWeight: 700 }}>
+					Publish your book
+				</Typography>
+				<DrizzleForm
+					ipfsHash={ipfsHash}
+					method='publishBook'
+					drizzle={drizzle}
+					drizzleState={drizzleState}
+					initialized={initialized}
+					setPublishOpen={setPublishOpen}
+				/>
+			</div>
+
+			<Divider />
+
+			<div className={classes.fileForm}>
+				<form onSubmit={onSubmit}>
+					<input
+						type='file'
+						accept='.epub'
+						onChange={captureFile}
+						style={{ display: 'none' }}
+						id='icon-button-file'
 					/>
+					<label htmlFor='icon-button-file'>
+						<Button
+							color='default'
+							aria-label='upload epub'
+							component='span'
+							variant='contained'
+							startIcon={<CloudUploadIcon />}>
+							Select epub file
+						</Button>
+					</label>
+					<Button
+						variant='contained'
+						color='default'
+						component='span'
+						onClick={onSubmit}
+						style={{ marginLeft: 20 }}>
+						Upload
+					</Button>
+				</form>
 
-					<h2>SimpleStorage</h2>
-					<p>
-						This shows a simple ContractData component with no arguments, along with a
-						form to set its value.
-					</p>
-				</div>
+				<Typography variant='h6'>selected filename: {epubFile}</Typography>
+			</div>
+			
+			<Divider />
 
-				<div>
-					<form onSubmit={onSubmit}>
-						<input
-							type='file'
-							accept='.epub'
-							onChange={captureFile}
-							style={{ display: 'none' }}
-							id='icon-button-file'
-						/>
-						<label htmlFor='icon-button-file'>
-							<Button
-								color='default'
-								aria-label='upload picture'
-								component='span'
-								variant='contained'
-								startIcon={<CloudUploadIcon />}>
-								Select epub file
-							</Button>
-						</label>
-						<div>
-							selected file: {epubFile}
-						</div>
-						<button variant='contained' color='default' component='span'>
-							Upload
-						</button>
-					</form>
-				</div>
-			</DrizzleForm>
-		</div>
+			<div className={classes.root}>
+				<Snackbar open={uploadOpen} autoHideDuration={6000} onClose={handleCloseUpload}>
+					<Alert onClose={handleCloseUpload} severity='success'>
+						Uploaded file successfully!
+					</Alert>
+				</Snackbar>
+				<Snackbar open={publishOpen} autoHideDuration={6000} onClose={handleClosePublish}>
+					<Alert onClose={handleClosePublish} severity='success'>
+						Published ebook successfully!
+					</Alert>
+				</Snackbar>
+			</div>
+		</Container>
 	)
 }
 
 const useStyles = makeStyles(theme => ({
+	formContainer: {},
 	container: {
-		marginTop: '20%',
-		backgroundColor: '#17141d',
+		// backgroundColor: '#17141d',
+		paddingTop: '10%',
 		height: '100%',
 		width: '100%',
-		color: '#e4e4e4',
+		color: '#141414',
+	},
+	fileForm: {
+		display: 'grid',
+		gridTemplateColumns: '1fr 1fr',
+		gap: 40,
+		marginTop: 40,
+		marginBottom: 40,
+	},
+	root: {
+		width: '100%',
+		'& > * + *': {
+			marginTop: theme.spacing(2),
+		},
 	},
 }))
--- client/src/Components/pages/DrizzleForm.js
+++ client/src/Components/pages/DrizzleForm.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { newContextComponents } from '@drizzle/react-components'
 import { DrizzleContext } from '@drizzle/react-plugin'
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core'
-
+import Container from '@material-ui/core/Container'
+import { Button, Input, makeStyles, TextField } from '@material-ui/core'
+import { Height } from '@material-ui/icons'
+import Paper from '@material-ui/core/Paper'
 const { AccountData, ContractData, ContractForm } = newContextComponents
 
-const DrizzleForm = ({ drizzle, drizzleState, initialized, method, children }) => {
+const DrizzleForm = ({ drizzle, drizzleState, initialized, method, children, ipfsHash, price, setPublishOpen }) => {
 	const classes = useStyles()
 
 	if (!initialized) {
@@ -16,9 +17,8 @@ const DrizzleForm = ({ drizzle, drizzleState, initialized, method, children }) =
 	const { accounts } = drizzleState
 
 	return (
-		<Container maxWidth="sm" className={classes.formContainer}>
+		<div>
 			{children}
-
 			<ContractForm
 				drizzle={drizzle}
 				drizzleState={drizzleState}
@@ -26,35 +26,62 @@ const DrizzleForm = ({ drizzle, drizzleState, initialized, method, children }) =
 				method={method}
 				labels={['IPFS Hash', 'Title', 'Price']}
 				render={({ inputs, inputTypes, state, handleInputChange, handleSubmit }) => (
-					<form onSubmit={handleSubmit}>
-						{inputs.map((input, index) => (
-							<input
-								style={{ fontSize: 30 }}
-								key={input.name}
-								type={inputTypes[index]}
-								name={input.name}
-								value={state[input.name]}
-								placeholder={input.name}
-								onChange={handleInputChange}
-							/>
-						))} 
-						<button
+					<form className={classes.formBody} onSubmit={handleSubmit}>
+						{inputs.map((input, index) => {
+							let value = input.name === '_source' ? ipfsHash : state[input.name]
+
+							if (method === 'purchaseBook' && input.name === '_price') {
+								value = price
+							}
+
+							return (
+								<TextField
+									variant='outlined'
+									color='secondary'
+									fullWidth
+									className={classes.textField}
+									key={input.name}
+									type={inputTypes[index]}
+									name={input.name}
+									value={value}
+									placeholder={input.name}
+									onChange={handleInputChange}
+								/>
+							)
+						})}
+						
+						<Button
 							key='submit'
 							type='button'
-							onClick={handleSubmit}
-							style={{ fontSize: 30 }}>
+							variant='contained'
+							color='default'
+							onClick={handleSubmit}>
 							Submit
-						</button>
+						</Button>
 					</form>
 				)}
 			/>
-		</Container>
+		</div>
 	)
 }
 
 export default DrizzleForm
 
 const useStyles = makeStyles(theme => ({
-	
+	formContainer: {
+		width: '100%',
+		height: '100%',
+		paddingTop: '10%',
+	},
+	formBody: {
+		marginTop: '20px',
+		marginBottom: '20px',
+	},
+	textField: {
+		marginTop: '15px',
+		marginBottom: '15px',
+		'& input': {
+			// color: '#e4e4e4',
+		},
+	},
 }))
-
